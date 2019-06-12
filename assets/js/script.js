cadf612116375581a5d3483bfe4cf7a5512b84bd
Zahadym <Zahadym@gmail.com>
2019-06-12 10:13:58 +0400

    10/13 12-06

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,6 @@ $(document).ready(function(){
 
     $('#next').on("click", function(event){
         $('#photo2').addClass('your--active');
+        $('#photo3').removeClass('your--active');
     });
 });

869224eee223921e2bd233570531909aad81e6bb
Zahadym <47822127+Zahadym@users.noreply.github.com>
2019-07-08 08:44:22 +0400

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
     /* Slider */
     $("[data-slider]").slick({
         infinite: true,
@@ -6,5 +6,20 @@ $(function() {
         slidesToScroll: 1
     });
 
-    $("body").addClass("animated fadeOut");
-  });  
+    $("[intro__slider]").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        speed: 500,
+        arrows: false
+    });
+
+    $('a[data-target^="anchor"]').bind('click.smoothscroll', function(){
+        var target = $(this).attr('href'),
+            bl_top = $(target).offset().top - 90;
+        $('body, html').animate({scrollTop: bl_top}, 700);
+        return false;
+    });
+
+});
